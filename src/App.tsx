b4676ebf99a25b1f674880bfa519fd4c7976d979
AlpacaUniix <supatsara_wan@cmu.ac.th>
2022-12-15 14:28:51 +0700

    del bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,8 @@ import * as Sentry from '@sentry/react'
 import { Router, Switch, Route } from 'react-router-dom'
 import { history } from 'utils/history';
 import { Loading } from './components/base/Loading';
-import AddTasks from 'components/base/InputTask';
+import AddTasks from 'components/base/newTask';
 import TableTodo from './components/base/Table';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const SentryRoute = Sentry.withSentryRouting(Route)
@@ -15,7 +14,7 @@ export default function App () {
     <Suspense fallback={<Loading initialized={false} />}>
    <Router history={history}>
     <Switch>
-      <SentryRoute exact path="/" component={Loading} />
+      <SentryRoute exact path="/load" component={Loading} />
       <SentryRoute exact path="/addtask" component={AddTasks} />
       <SentryRoute exact path="/table" component={TableTodo} />
        </Switch>
