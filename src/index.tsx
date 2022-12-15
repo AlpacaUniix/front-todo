7a96a62f3fe22ff09add7764e9d08df296dc72b9
AlpacaUniix <supatsara_wan@cmu.ac.th>
2022-12-15 14:30:04 +0700

    add tailwindcss / del bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,7 @@ import * as Sentry from '@sentry/react'
 import { createBrowserHistory } from 'history'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import 'animate.css/animate.min.css'
-import 'react-datepicker/dist/react-datepicker.css'
-import 'react-image-crop/dist/ReactCrop.css'
-import 'react-toastify/dist/ReactToastify.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import './index.css'
-
-import { DB_URL } from './config/env';
 
 const history = createBrowserHistory()
 
