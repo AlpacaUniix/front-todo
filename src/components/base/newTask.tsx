2f992ba612d90040785e5f79b2d3a1e479495900
AlpacaUniix <supatsara_wan@cmu.ac.th>
2022-12-16 15:57:26 +0700

    add comment

--- src/components/base/newTask.tsx
+++ src/components/base/newTask.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { postTasks } from '../../services/request'
-// import TableTodo from './Table'
+import TableTodo from './Table'
+
 
 
 export default function AddTask() {
@@ -10,8 +11,6 @@ export default function AddTask() {
     handleSubmit,
     formState: { errors },
   } = useForm()
-
-
 
   const addData = async (data) => {
     console.log(data)
@@ -28,9 +27,11 @@ export default function AddTask() {
     <div className="bg-pink-300 w-full font-mono flex justify-center">
       <div className="w-4/5">
         <form onSubmit={handleSubmit(addData)}>
+          {/* Header Text */}
           <h2 className="pt-5 text-center text-3xl text-purple-300">
             To Do List
           </h2>
+          {/* input Task */}
           <span className="block mx-2 text-sm font-medium text-purple-300">
             Task name
           </span>
@@ -42,12 +43,12 @@ export default function AddTask() {
               focus:ring-blue-200 block w-full rounded-md sm:text-sm focus:ring-1"
               {...register('task', { required: true, maxLength: 80 })}
           />
+          {/* input Date */}
           <span className="block mx-2 mt-3 text-sm font-medium text-purple-300">
             Date
           </span>
           <input
-            
-            type="text"
+            type="date"
             placeholder="yy-mm-dd (2021-10-01)"
             className="mt-1 px-3 py-2 bg-white border shadow-sm text-blue-200
              border-blue-100 placeholder-blue-100 focus:outline-none focus:border-blue-200
@@ -55,6 +56,7 @@ export default function AddTask() {
               {...register('date', { required: true, maxLength: 80 })}
           />
           {errors.exampleRequired && <p>This field is required</p>}
+          {/* Button add task */}
           <div className="flex justify-center">
             <button
               className="p-2 mt-4 rounded-md  text-white
@@ -64,8 +66,8 @@ export default function AddTask() {
               Add
             </button>
           </div>
-          
         </form>
+        <TableTodo />
       </div>
     </div>
   )
